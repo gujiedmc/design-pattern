58137876cbce7bb6a7d5ed3746b1618067b2d0c2
dyc <gujiedmc@gmail.com>
2020-04-13 18:13:42 +0800

    适配器模式

---
+++ adapter/src/main/java/com/gujiedmc/study/adapter/AdapterTest.java
@@ -0,0 +1,19 @@
+package com.gujiedmc.study.adapter;
+
+/**
+ * @author gujiedmc
+ * @date 2020/4/11
+ */
+public class AdapterTest {
+
+    public static void main(String[] args) {
+        Usb usb = new UsbDisk("usb接口U盘中的信息");
+
+        Computer computer = new Computer();
+        computer.readDiskData(usb);
+
+        TypeC typeC = new TypeCDisk("type-c接口u盘中的信息");
+        // Computer不支持TypeC接口，用转换器适配
+        computer.readDiskData(new TypeC2UsbAdapter(typeC));
+    }
+}
---
+++ adapter/src/main/java/com/gujiedmc/study/adapter/Usb.java
@@ -0,0 +1,12 @@
+package com.gujiedmc.study.adapter;
+
+/**
+ * USB接口
+ *
+ * @author gujiedmc
+ * @date 2020/4/11
+ */
+public interface Usb {
+
+    String read();
+}
